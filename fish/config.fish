3e49e6f64dbaa0c79bd081066dec3b84878540d0
Bektosh Madaminov <madaminov.b2@rwb.ru>
2025-05-19 16:50:42 +0500

    replaced fish config with the one updated for Arch Linux

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,15 @@
-/opt/homebrew/bin/brew shellenv | source
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -d (brew --prefix)"/share/fish/completions"
-    set -p fish_complete_path (brew --prefix)/share/fish/completions
-end
+if test (uname -s) = Darwin
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
 
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
 end
 
 # remove greeting message
@@ -22,20 +22,15 @@ set fish_cursor_insert block
 fish_config theme choose "Catppuccin Mocha"
 
 ## settings for PATH variable
-fish_add_path ~/.bun/bin
-fish_add_path /opt/homebrew/bin
+# fish_add_path ~/.bun/bin
 fish_add_path "$(go env GOPATH)/bin"
 fish_add_path ~/.local/bin/
-fish_add_path /Users/rock/Library/Python/3.9/bin
+fish_add_path ~/.zig/
 
 # Other global env variables
-
-## C/C++ variables
-set -gx CC /usr/bin/clang
-set -gx CXX /usr/bin/clang++
+set -g GOPATH "$(go env GOPATH)"
+set -g GOPRIVATE "gitlab.wildberries.ru"
+set -g GONOPROXY "gitlab.wildberries.ru"
+set -g GONOSUMDB "gitlab.wildberries.ru"
 
 fzf --fish | source
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
