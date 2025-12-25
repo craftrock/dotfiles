c346bd189e4e81a3f97b87a9f71b3c781c9729e8
craftrock <bektoshmadaminov001@gmail.com>
2025-12-25 23:03:14 +0500

    last sync of pc

--- fish/.config/fish/conf.d/010-env.fish
+++ fish/.config/fish/conf.d/010-env.fish
@@ -2,6 +2,9 @@ fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
 fish_add_path ~/.cargo/bin
 fish_add_path /usr/lib/qt6/bin
+# opencode
+fish_add_path ~/.opencode/bin
 
 set --global --export EDITOR nvim
 set --global --export STARSHIP_CONFIG ~/.config/starship/starship.toml
+set --global --export SSH_AUTH_SOCK ~/.1password/agent.sock
