ac397111843197eb16ced49e49967c7149364c6a
Bektosh Madaminov <bektoshmadaminov001@gmail.com>
2026-01-16 00:10:43 +0500

    snacks explorer changes + tmux color changes

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,14 @@ return {
       sources = {
         explorer = {
           hidden = true, -- show hidden files by default
+          layout = {
+            layout = {
+              position = "right",
+              width = 0.3,
+              min_width = 40,
+              max_width = 60,
+            },
+          },
         },
       },
     },
