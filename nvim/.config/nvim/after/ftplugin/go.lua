5908ac55a55168d870fc9b8c666312ba0ff935f3
Bektosh Madaminov <bektoshmadaminov001@gmail.com>
2026-01-19 01:19:03 +0500

    fix golang tab showing

---
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -0,0 +1,13 @@
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = false -- Use actual tabs (Go convention)
+vim.opt_local.list = false
+
+-- Enforce list=false after plugins (they keep overriding it)
+vim.api.nvim_create_autocmd("BufEnter", {
+  buffer = 0,
+  callback = function()
+    vim.opt_local.list = false
+  end,
+})
--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,17 +7,6 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
---- Go-specific indentation settings
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "go",
-  callback = function()
-    vim.opt_local.tabstop = 4 -- Number of spaces a tab counts for
-    vim.opt_local.shiftwidth = 4 -- Number of spaces for each indentation level
-    vim.opt_local.softtabstop = 4 -- Number of spaces for tab in insert mode
-    vim.opt_local.expandtab = false -- Use actual tabs (Go convention)
-  end,
-})
-
 --- C/C++ indentation settings
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "c", "cpp" },
