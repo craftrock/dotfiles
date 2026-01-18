79a1442648634bc706f3a7bd2cb4bf1e84a1baaa
Bektosh Madaminov <bektoshmadaminov001@gmail.com>
2026-01-18 15:53:51 +0500

    disable matching words highlight in snacks

--- fish/.config/fish/conf.d/010-env.fish
+++ fish/.config/fish/conf.d/010-env.fish
@@ -4,6 +4,7 @@ fish_add_path ~/.cargo/bin
 fish_add_path /usr/lib/qt6/bin
 fish_add_path ~/.opencode/bin
 fish_add_path ~/.bun/bin
+fish_add_path ~/clones/Odin/
 
 set --global --export EDITOR nvim
 set --global --export STARSHIP_CONFIG ~/.config/starship/starship.toml
