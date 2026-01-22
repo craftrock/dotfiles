a6535ca5043a17367984e9d858302b6599c54a46
Bektosh Madaminov <bektoshmadaminov001@gmail.com>
2026-01-23 02:58:22 +0500

    icons: add odin icon for files

---
+++ nvim/.config/nvim/lua/plugins/icons.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-mini/mini.icons",
+    opts = {
+      extension = {
+        odin = { glyph = "󰟢", hl = "MiniIconsBlue" },
+      },
+      filetype = {
+        odin = { glyph = "󰟢", hl = "MiniIconsBlue" },
+      },
+    },
+  },
+}
